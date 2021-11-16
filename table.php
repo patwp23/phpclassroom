c693f8b81e604b686a969826aa3327ed037d1658
patwp23 <89084406+patwp23@users.noreply.github.com>
2021-11-16 09:11:28 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,19 +1,15 @@
-<html>
-  <head></head>
-  <body>
-    <table border="1">
-      <tr>
-        <td>column 1</td>
-        <td>column 2</td>
-        <td>column 3</td>
-        <td>column 4</td>
-       </tr>
-       <tr>
-        <td>column 1</td>
-        <td>column 2</td>
-        <td>column 3</td>
-        <td>column 4</td>
-       </tr>
-      </table>
-    </body>
-  </html>
+<?php>
+  echo "<table border='1'>"; //tagเปิด
+
+  for($i=0;$i<10;$i++){
+    echo "<tr>";
+    echo "<td>column</td>";
+    echo "<td>column</td>";
+    echo "<td>column</td>";
+    echo "<td>column</td>";
+    echo "</tr>";
+  }
+
+  echo "</table>";  //tagปิด
+
+?>
