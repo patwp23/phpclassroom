425ef9a96329f4f3499dfaac6fe9b15b739d01b7
patwp23 <89084406+patwp23@users.noreply.github.com>
2021-12-21 09:25:37 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,7 +3,7 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-  echo "<center><h4>รายงานสถานการโควิด-19 แยกจังหวัด</h4></center>"
+  echo "<p><center><h4>รายงานสถานการโควิด-19 แยกจังหวัด</h4></center></p>";
   echo "<table border='1'>";
   echo "<tr><td>ลำดับ</td><td>จังหวัด</td><td>ผู้ป่วยใหม่</td><td>ผู้ป่วยรวม</td><td>ผู้ป่วยใหม่ในประเทศ</td><td>ผู้ป่วยรวมในประเทศ</td><td>ผู้เสียชีวิตใหม่</td><td>ผู้เสียชีวิตรวม</td><td>วันที่อัพเดท</td></tr>";
   foreach($data as $key=>$val){
