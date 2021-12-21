02198f62aede92034b0db823151c6958fbf6dd43
patwp23 <89084406+patwp23@users.noreply.github.com>
2021-12-21 10:34:43 +0700

    Update province.php

--- province.php
+++ province.php
@@ -6,9 +6,10 @@
 
   echo "<p><center><h2>รายงานสถานการโควิด-19 แยกจังหวัด</h2></center></p>";
   echo "<p><center><table border='1'>";
-  echo "<tr><td style = 'background-color:#5092B4'><center><b>ลำดับ</b></center></td><td><center><b>จังหวัด</b></center></td><td><center><b>ผู้ป่วยใหม่</b></center></td><td><center><b>ผู้ป่วยรวม</b></center></td>
+  echo "<tr><td style = 'background-color:#5092B4'><center><b>ลำดับ</b></center></td><td style = 'background-color:#5092B4'><center><b>จังหวัด</b></center></td>
+  <td style = 'background-color:#5092B4'><center><b>ผู้ป่วยใหม่</b></center></td><td style = 'background-color:#5092B4'><center><b>ผู้ป่วยรวม</b></center></td>
   <td><center><b>ผู้ป่วยใหม่ในประเทศ</b></center></td><td><center><b>ผู้ป่วยรวมในประเทศ</b></center></td><td><center><b>ผู้เสียชีวิตใหม่</b></center></td>
-  <td><center><b>ผู้เสียชีวิตรวม</b></center></td><td><center><b>วันที่อัพเดท</b></center></td></tr>";
+  <td style = 'background-color:#5092B4'><center><b>ผู้เสียชีวิตรวม</b></center></td><td style = 'background-color:#5092B4'><center><b>วันที่อัพเดท</b></center></td></tr>";
   foreach($data as $key=>$val){
     echo "<tr>";
     echo "<td>";
