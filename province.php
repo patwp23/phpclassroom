d70d0bf37043d31e079d344e734f663ab483b7d0
patwp23 <89084406+patwp23@users.noreply.github.com>
2021-12-21 09:45:51 +0700

    Update province.php

--- province.php
+++ province.php
@@ -5,7 +5,7 @@
   
 
   echo "<p><center><h3>รายงานสถานการโควิด-19 แยกจังหวัด</h3></center></p>";
-  echo "<p><center><table border='1' bgcolor="pink">";
+  echo "<p><center><table border='1' bgcolor='pink'>";
   echo "<tr><td><center><b>ลำดับ</b></center></td><td><center><b>จังหวัด</b></center></td><td><center><b>ผู้ป่วยใหม่</b></center></td><td><center><b>ผู้ป่วยรวม</b></center></td>
   <td><center><b>ผู้ป่วยใหม่ในประเทศ</b></center></td><td><center><b>ผู้ป่วยรวมในประเทศ</b></center></td><td><center><b>ผู้เสียชีวิตใหม่</b></center></td>
   <td><center><b>ผู้เสียชีวิตรวม</b></center></td><td><center><b>วันที่อัพเดท</b></center></td></tr>";
