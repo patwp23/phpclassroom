9cb95b6dc666ecb7cf72a5549296f81e36630593
patwp23 <89084406+patwp23@users.noreply.github.com>
2022-01-11 09:06:30 +0700

    Update style.php

--- style.php
+++ style.php
@@ -1,16 +1,22 @@
 <html>
   <head>
-    <style>
-      #box1{
-        background-color: red;
-        width: 50px;
-        height: 50px;
-      }
-    </style>
+   <style>
+     .g{
+        background-color: green;
+     }
+     #box1{
+        width:50px;
+        height:50px;
+     }
+     #box2{
+        width:100px;
+        height:50px;
+     }
+   </style>
   </head>
   <body>
-    <div id="box1">
-      
-    </div>
+    <div class="g" id="box1"></div>
+    <br><br><br><br>
+    <div  class="g" id="box2"> </div>
   </body>
 </html>
