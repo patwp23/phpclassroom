2912cdcaa249cf60760e47538f56fece9fb916e1
patwp23 <89084406+patwp23@users.noreply.github.com>
2022-01-11 09:25:21 +0700

    Update style.php

--- style.php
+++ style.php
@@ -2,21 +2,26 @@
   <head>
    <style>
      .g{
-        background-color: green;
+        background-color: lightblue;
      }
      #box1{
-        width:50px;
-        height:50px;
+        width:250px;
+        height:250px;
+        margin-bottom: 10px;
+        padding : 20px;
      }
      #box2{
-        width:100px;
-        height:50px;
+        width:400px;
+        height:250px;
      }
    </style>
   </head>
   <body>
-    <div class="g" id="box1"></div>
-    <br><br><br><br>
-    <div  class="g" id="box2"> </div>
+    <div class="g" id="box1">
+      เบอร์เกอร์สังโฆสปายบอมบ์
+    </div>
+    <div  class="g" id="box2"> 
+      เบอร์เกอร์สังโฆสปายบอมบ์คอปเตอร์น้องใหม่แจ็กเก็ต
+    </div>
   </body>
 </html>
