0f163f48d549fc83ee09f11f0662813139f794d3
patwp23 <89084406+patwp23@users.noreply.github.com>
2022-01-18 11:25:17 +0700

    Update project.php

--- project.php
+++ project.php
@@ -135,7 +135,7 @@
           <!-- แก้ไขส่วนที่ 3 -->
           <!-- Loop -->
            <?php 
-              $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
+              $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
               $data = json_decode($json);
              
             foreach($data as $key=>$val){
